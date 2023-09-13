cf3e855433f14db053fa7c50d844332db1beace7
Hoe Ze Il <hoezeil20@gmail.com>
2023-09-13 23:46:33 +0700

    made page responsive

--- src/components/forecast/Forecast.jsx
+++ src/components/forecast/Forecast.jsx
@@ -3,47 +3,47 @@ import ForecastCSS from "./Forecast.module.css";
 function Forecast() {
   return (
     <div className={ForecastCSS.container}>
-      <p className={ForecastCSS["hourly-text"]}>Hourly Forecast</p>
+      <p className={ForecastCSS["forecast-text"]}>Hourly Forecast</p>
       <div className={ForecastCSS["line-row"]}></div>
-      <div className={ForecastCSS["hourly-forecasts"]}>
-        <div className={ForecastCSS["hourly-forecast"]}>
-          <p className={ForecastCSS.hourly}>11 AM</p>
+      <div className={ForecastCSS["forecasts"]}>
+        <div className={ForecastCSS["forecast"]}>
+          <p className={ForecastCSS.forecast}>11 AM</p>
           <img
-            className={ForecastCSS["hourly-icon"]}
+            className={ForecastCSS["forecast-icon"]}
             alt="weather icon"
             src="./img/01d.png"
           />
-          <p className={ForecastCSS["hourly-celsius"]}>31ºC</p>
+          <p className={ForecastCSS["forecast-celsius"]}>31ºC</p>
         </div>
         <div className={ForecastCSS["line-col"]}></div>
-        <div className={ForecastCSS["hourly-forecast"]}>
-          <p className={ForecastCSS.hourly}>11 AM</p>
+        <div className={ForecastCSS["forecast"]}>
+          <p className={ForecastCSS.forecast}>11 AM</p>
           <img
-            className={ForecastCSS["hourly-icon"]}
+            className={ForecastCSS["forecast-icon"]}
             alt="weather icon"
             src="./img/01d.png"
           />
-          <p className={ForecastCSS["hourly-celsius"]}>31ºC</p>
+          <p className={ForecastCSS["forecast-celsius"]}>31ºC</p>
         </div>
         <div className={ForecastCSS["line-col"]}></div>
-        <div className={ForecastCSS["hourly-forecast"]}>
-          <p className={ForecastCSS.hourly}>11 AM</p>
+        <div className={ForecastCSS["forecast"]}>
+          <p className={ForecastCSS.forecast}>11 AM</p>
           <img
-            className={ForecastCSS["hourly-icon"]}
+            className={ForecastCSS["forecast-icon"]}
             alt="weather icon"
             src="./img/01d.png"
           />
-          <p className={ForecastCSS["hourly-celsius"]}>31ºC</p>
+          <p className={ForecastCSS["forecast-celsius"]}>31ºC</p>
         </div>
         <div className={ForecastCSS["line-col"]}></div>
-        <div className={ForecastCSS["hourly-forecast"]}>
-          <p className={ForecastCSS.hourly}>11 AM</p>
+        <div className={ForecastCSS["forecast"]}>
+          <p className={ForecastCSS.forecast}>11 AM</p>
           <img
-            className={ForecastCSS["hourly-icon"]}
+            className={ForecastCSS["forecast-icon"]}
             alt="weather icon"
             src="./img/01d.png"
           />
-          <p className={ForecastCSS["hourly-celsius"]}>31ºC</p>
+          <p className={ForecastCSS["forecast-celsius"]}>31ºC</p>
         </div>
       </div>
     </div>
